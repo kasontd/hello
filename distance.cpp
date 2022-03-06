05ebedd09ea4b0019673305cb37df4c9c6ba590c
kasontd <100179774+kasontd@users.noreply.github.com>
2022-03-06 20:38:34 +0700

    Add files via upload

---
+++ distance.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int x, y;
+    double D;
+    cin >> x >> y;
+    D = sqrt(x*x+y*y);
+    cout << D;
+    return 0;
+}
